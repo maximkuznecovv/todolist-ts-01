1ece70b1ee18bebf0cae5fd5f18b41c7d7770ee2
maximkuznecovv <maximkuznecovv@gmail.com>
2021-05-30 17:37:37 +0300

    15 - preloader

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,30 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import {
     AppBar,
     Button,
     Container,
     createStyles,
-    IconButton,
+    IconButton, LinearProgress,
     makeStyles,
     Theme,
     Toolbar,
     Typography
-} from '@material-ui/core';
-import {Menu} from '@material-ui/icons';
-import {TodoListsList} from '../features/TodoListsList/TodoListsList';
+} from "@material-ui/core";
+import {Menu} from "@material-ui/icons";
+import {TodoListsList} from "../features/TodoListsList/TodoListsList";
+import {useSelector} from "react-redux";
+import {AppRootStateType} from "./store";
+import {RequestStatusType} from "./app-reducer";
+import {ErrorSnackbar} from "../components/ErrorSnackbar";
 
 export function App() {
+
+    const status = useSelector<AppRootStateType, RequestStatusType>(state => state.app.status)
+
     return (
         <div className="App">
+            <ErrorSnackbar/>
             <AppBar position="static">
                 <Toolbar>
                     <IconButton edge="start" color="inherit" aria-label="menu">
@@ -28,8 +36,9 @@ export function App() {
                     <Button color="inherit">Login</Button>
                 </Toolbar>
             </AppBar>
+            {status === 'loading' && <LinearProgress color="secondary"/>}
             <Container fixed>
-                <TodoListsList />
+                <TodoListsList/>
             </Container>
         </div>
     );
